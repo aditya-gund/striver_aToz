d94fa538bca2c4c16ceb64fb103b57b93df64820
Aditya <adityagundg14@gmail.com>
2024-07-29 22:49:23 +0530

    Pattern4

--- Basics/Patterns/Patterns.java
+++ Basics/Patterns/Patterns.java
@@ -1,6 +1,6 @@
 class Patterns{
     public static void main(String[] args) {
-        Pattern3 pt = new Pattern3();
-        pt.pattern3();
+        Pattern4 pt = new Pattern4();
+        pt.pattern4();
     }
 }
