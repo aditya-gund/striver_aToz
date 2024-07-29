0b8795f444e444c452277ba73f3d2964e41cab59
Aditya <adityagundg14@gmail.com>
2024-07-29 22:39:34 +0530

    Pattern1

---
+++ Basics/Patterns/Pattern1.java
@@ -0,0 +1,17 @@
+public class Pattern1 {
+    /*
+    *****
+    *****
+    *****
+    *****
+    ***** 
+     */
+    public static void pattern1(){
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Basics/Patterns/Patterns.java
@@ -0,0 +1,6 @@
+class Patterns{
+    public static void main(String[] args) {
+        Pattern1 pt = new Pattern1();
+        pt.pattern1();
+    }
+}
